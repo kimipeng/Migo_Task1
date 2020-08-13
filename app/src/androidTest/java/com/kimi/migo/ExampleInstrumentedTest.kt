62a630fc2468cf36b1003e6458a3f209776f1fbb
kimipeng <pon0306@gmail.com>
2020-08-13 18:58:35 +0800

    Add UITest.

--- app/src/androidTest/java/com/kimi/migo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kimi/migo/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.kimi.migo
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,13 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kimi.migo", appContext.packageName)
+    fun showTaskActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.q1)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(TaskActivity::class.java.name))
     }
 }
